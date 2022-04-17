acee3b0402bad5153ee497d89341fef8f2e89ce4
Manoj Kumar <70743261+manojkumar8400@users.noreply.github.com>
2022-04-17 11:56:16 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,7 @@ import { Notes } from "./CreateNotes/Notes";
 import { Label } from "./Label/Label";
 import { Archieve } from "./Archieve/Archieve";
 import { Trash } from "./Trash/Trash";
-
-export { Home, Notes, Label, Archieve, Trash }
 import { Login } from "./Auth/Login";
 import { Signup } from "./Auth/Signup";
 
-export { Home, Notes, Login, Signup }
+export { Home, Notes, Label, Archieve, Trash, Login, Signup }
