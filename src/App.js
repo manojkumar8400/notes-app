52df312ff79b2a2960b7ed90f6f71f188419f2a4
Manoj Kumar <70743261+manojkumar8400@users.noreply.github.com>
2022-04-17 11:50:18 +0530

    fixed miner bugs.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "../src/components/Utilities/Utilities.css";
 import "./App.css";
-import { Home, Notes,Archieve, Label, Trash } from "./pages/index";
-import { Navigation } from "./components/index";
-import { Home, Notes, Login, Signup } from "./pages/index";
+import { Home, Notes,Archieve, Label, Trash, Login, Signup } from "./pages/index";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navigation/> */}
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="notes" element={<Notes/>} /> 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,6 +10,7 @@ const Navigation = () => {
             <nav className="nav-bar flex jstfy-ctnt-spc-between position-sticky z-index">
                 <div className='logo-design flex flex-direction-column'>
                     <Link to="/" className="logo"><span> Notejoy </span></Link>
+                </div>
                 <div className='logo-design flex'>
                     <Link to="/" className="logo"><span>Notejoy</span> </Link>
                 </div>
